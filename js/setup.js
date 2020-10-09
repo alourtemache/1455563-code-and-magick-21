6af503f23ef7f5443b95c3c733ea8d8b76d43f1a
Alina <64503919+alourtemache@users.noreply.github.com>
2020-10-09 12:20:28 +0100

    Поправлен массив

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const WIZARD_NAMES = ['Вашингтон Вальц', 'Иван Верон', 'Мария Мирабелла', 'Кристоф Ирвинг'];
+const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const WIZARD_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD_EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 
 let userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -13,24 +16,24 @@ let similarWizardTemplate = document.querySelector('#similar-wizard-template')
 
 let wizards = [
   {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(215, 210, 55)',
-    eyesColor: 'blue',
+    name: WIZARD_NAMES[0] + WIZARD_SURNAMES[0],
+    coatColor: WIZARD_COAT_COLOR[0],
+    eyesColor: WIZARD_EYES_COLOR[0],
   },
   {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(146, 100, 161)',
-    eyesColor: 'red',
+    name: WIZARD_NAMES[1] + WIZARD_SURNAMES[1],
+    coatColor: WIZARD_COAT_COLOR[1],
+    eyesColor: WIZARD_EYES_COLOR[1],
   },
   {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(56, 159, 117)',
-    eyesColor: 'yellow',
+    name: WIZARD_NAMES[2] + WIZARD_SURNAMES[2],
+    coatColor: WIZARD_COAT_COLOR[2],
+    eyesColor: WIZARD_EYES_COLOR[2],
   },
   {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(101, 137, 164)',
-    eyesColor: 'green',
+    name: WIZARD_NAMES[3] + WIZARD_SURNAMES[3],
+    coatColor: WIZARD_COAT_COLOR[3],
+    eyesColor: WIZARD_EYES_COLOR[3],
   }
 ];
 
